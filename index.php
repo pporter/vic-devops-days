12467cbb8f2c7e2880785cbc5f3d2cc4637a6439
Saqib Khan <skhan@xmatters.com>
2019-06-05 15:24:23 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<h1>500 Internal Server Error!!</h1>
+body, html {
+  height: 100%;
+}
+
+.bg { 
+  /* The image used */
+  background-image: url("https://www.centralsmith.ca/template/style/img/ABOUTUS_BACKGROUND.jpg");
+
+  /* Full height */
+  height: 100%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
